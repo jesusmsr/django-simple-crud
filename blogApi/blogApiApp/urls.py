e7f3378617f67a445ccdb97e615153d1cef6cd9f
jesusmsr <jsr20xx@gmail.com>
2023-10-02 11:59:23 +0200

    first endpoint

--- blogApi/blogApiApp/urls.py
+++ blogApi/blogApiApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('api/get-all-posts/', views.get_all_posts)
 ]
--- blogApi/blogApiApp/views.py
+++ blogApi/blogApiApp/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-# Create your views here.
-
+from .models import Post
+from .serializers import PostSerializer
 
 @api_view(['GET'])
 def index(request):
     return Response({"Success": "Index endpoint working"})
+
+@api_view(['GET'])
+def get_all_posts(request):
+    all_posts = Post.objects.all()
+    serializer = PostSerializer(all_posts, many=True)
+
+    return Response(serializer.data)
